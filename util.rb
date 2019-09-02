dbaea96e02bc35ff20d51cfda30a7b569a89a145
Oscar Heller <oheller@protonmail.com>
2019-09-01 20:19:40 -0400

    Moved String functions to util.rb.

--- util.rb
+++ util.rb
@@ -9,3 +9,35 @@ class Object
     end
   end
 end
+
+class String
+  def to_a
+    [ self ]
+  end
+
+  def fuzzy_match( arg )
+    self.match(/\A#{arg}.*\z/i)
+  end
+
+  def capitalize_first
+    slice(0, 1).to_s.capitalize + slice(1..-1).to_s
+  end
+
+    def sanitize
+        self.gsub(/[\%\[\]\^]/, "")
+    end
+end
+
+module Position
+    SLEEP = 0
+    REST = 1
+    STAND = 2
+    FIGHT = 3
+
+    STRINGS = [
+        "sleeping",
+        "resting",
+        "standing",
+        "fighting"
+    ]
+end
