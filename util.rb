74560b0f5a78fd05cf65e0fb6de82853d4ac3594
Oscar Heller <oheller@protonmail.com>
2019-09-01 20:19:22 -0400

    Cleaned up instance_options_try.

--- util.rb
+++ util.rb
@@ -1,15 +1,11 @@
 class Object
   def instance_options_try(options)
     options.each do |key, value|
-      self.instance_variable_try(key, value)
-    end
-  end
-
-  def instance_variable_try(key, value)
-    if instance_variable_defined?("@#{key}")
-      self.instance_variable_set("@#{key}", value)
-    else
-      puts "Illegal instance variable tried [key: #{key}, value: #{value}]."
+      if instance_variable_defined?("@#{key}")
+        self.instance_variable_set("@#{key}", value)
+      else
+        puts "Illegal instance variable tried [key: #{key}, value: #{value}]."
+      end
     end
   end
 end
