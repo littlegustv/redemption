767cc981e5edc0361d2bde3464ba0543b4e97b9c
Benny Heller <echo29x@gmail.com>
2019-10-04 00:47:48 -0400

    fixed command regex to allow command args to have non-letter characters, and to allow trailing single quote

--- mobile.rb
+++ mobile.rb
@@ -184,8 +184,9 @@ class Mobile < GameObject
     end
 
     def do_command( input )
-        cmd, args = input.sanitize.split " ", 2
-        @game.do_command( self, cmd, args.to_s.scan(/(((\d+|all)\*)?((\d+|all)\.)?(\w+|'[\w\s]+'))/i).map(&:first).map{ |arg| arg.gsub("'", "") } )
+        cmd, args = input.sanitize.split " ", 2        
+        @game.do_command( self, cmd, args.to_s.scan(/(((\d+|all)\*)?((\d+|all)\.)?([^\s\.\'\*]+|'[\w\s]+'?))/i).map(&:first).map{ |arg| arg.gsub("'", "") } )
+        # @game.do_command( self, cmd, args.to_s.scan(/(((\d+|all)\*)?((\d+|all)\.)?(\w+|'[\w\s]+'))/i).map(&:first).map{ |arg| arg.gsub("'", "") } )
     end
 
     # When mobile is attacked, respond automatically unless already in combat targeting someone else
